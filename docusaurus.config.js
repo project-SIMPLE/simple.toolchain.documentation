80483b684cb090c9a90fb384a12e139c8b7c5b93
RoiArthurB <contact@arthurbrugiere.fr>
2023-12-11 15:10:08 +0700

    [GHA] use docusaurus' deploy workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'project-SIMPLE', // Usually your GitHub org/user name.
   projectName: 'documentation', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
